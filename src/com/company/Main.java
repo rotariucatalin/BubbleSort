99563d9103dfcc22a1da57c14c09d8ca4cb7650a
catalin90 <catalin325815>
2021-01-18 14:02:01 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    private void bubbleSOrt(int[] arr) {
+
+        int n = arr.length;
+
+        for(int i = 0; i < n-1; i++) {
+
+            for(int j = 0; j < n-1-i; j++) {
+
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+
+
+    }
+}
